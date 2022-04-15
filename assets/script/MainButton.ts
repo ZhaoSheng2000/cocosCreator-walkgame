1281ad8c2339308851f608a0a1635abd45582fbb
zhaosheng <zs17337825170@163.com>
2022-04-15 14:48:27 +0800

    add login

---
+++ assets/script/MainButton.ts
@@ -0,0 +1,42 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+
+    // LIFE-CYCLE CALLBACKS:
+    onClick(){
+        
+        if (cc.sys.platform === cc.sys.WECHAT_GAME) {
+            let userinfo =  window['wx'].getStorageSync('userinfo');
+            let openid = window['wx'].getStorageSync('openid');
+            if(userinfo && openid){
+                console.log('userinfo',userinfo);
+            }else{
+                window['wx'].showToast({title:'请先登录',icon:'none'});
+                window['wx'].cloud.callFunction({
+                   name: 'login',
+                   data: {},
+                   success: res => {
+                        console.log('callFunction  result: ', res.result.openid)
+                        window['wx'].setStorageSync('openid', res.result.openid);
+                       }
+                    })
+                //授权弹窗
+                window['wx'].getUserProfile({
+                    desc: '用于完善资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+                    success: (res) => {
+                      console.log(res.userInfo);
+                        window['wx'].setStorageSync('userinfo', res.userInfo);
+                    }
+                  })
+                }
+            }
+    }
+
+    start () {
+    }
+
+    // update (dt) {}
+}
